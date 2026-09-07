acd729fdfe7bf17978c5f2ad257c2f1319f48561
wbz <wbz21@mails.tsinghua.edu.cn>
2026-09-07 21:01:11 +0800

    Support external model paths

--- skel/config.py
+++ skel/config.py
@@ -2,7 +2,26 @@ import os
 
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
-skel_folder = os.path.join(package_directory, '../data/skel')
-smpl_folder = os.path.join(package_directory, '../data/')
-fitting_mask_file = os.path.join(package_directory, 'alignment/riggid_parts_mask.pkl')
-default_config_file = os.path.join(package_directory, 'alignment/config.yaml')
+skel_folder = os.path.expanduser(
+    os.environ.get(
+        "SKEL_MODEL_PATH",
+        os.path.join(package_directory, "../data/skel"),
+    )
+)
+
+smpl_folder = os.path.expanduser(
+    os.environ.get(
+        "SMPL_MODEL_PATH",
+        os.path.join(package_directory, "../data"),
+    )
+)
+
+fitting_mask_file = os.path.join(
+    package_directory,
+    "alignment/riggid_parts_mask.pkl",
+)
+
+default_config_file = os.path.join(
+    package_directory,
+    "alignment/config.yaml",
+)
